ee8bd3458d4767d05770e3c9785247885a220020
adithya-retheep <adithyar.rba2327@saintgits.org>
2026-02-01 05:22:16 +0530

    Update vulnerable_1.py

--- vulnerable_1.py
+++ vulnerable_1.py
@@ -1,1 +1,10 @@
-clear
+import sqlite3
+
+def get_user(username):
+    conn = sqlite3.connect("users.db")
+    cursor = conn.cursor()
+
+    query = "SELECT * FROM users WHERE name = '" + username + "'"
+    cursor.execute(query)
+
+    print("User:", username)
